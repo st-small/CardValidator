d595e8f72fbc56e78c95cfa5127534252ab7656d
Станислав Шияновский <st-small@rambler.ru>
2019-11-14 19:25:31 +0200

    Initial Commit

---
+++ CardValidator/CardValidator.h
@@ -0,0 +1,19 @@
+//
+//  CardValidator.h
+//  CardValidator
+//
+//  Created by Станислав Шияновский on 11/14/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardValidator.
+FOUNDATION_EXPORT double CardValidatorVersionNumber;
+
+//! Project version string for CardValidator.
+FOUNDATION_EXPORT const unsigned char CardValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardValidator/PublicHeader.h>
+
+
